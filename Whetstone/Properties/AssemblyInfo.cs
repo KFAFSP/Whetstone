657758101f522476aea521dd866cf581a7624bef
Karl Friebel <karl.friebel@friebelnet.de>
2017-11-26 21:28:29 +0100

    Created core assembly and tests project.

---
+++ Whetstone/Properties/AssemblyInfo.cs
@@ -0,0 +1,23 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+
+#region Assembly description
+[assembly: AssemblyTitle("Whetstone")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Whetstone")]
+[assembly: AssemblyCopyright("")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+#endregion
+
+#region Friend assemblies
+// The test assembly is a friend.
+[assembly: InternalsVisibleTo("Whetstone.Tests")]
+#endregion
+
+#region Version
+[assembly: AssemblyVersion("0.2.*")]
+[assembly: AssemblyFileVersion("0.2.0.0")]
+#endregion
