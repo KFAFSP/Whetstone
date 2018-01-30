3ca240f3480cb6eb279edd8cf6bef2537f57a6cd
Karl Friebel <karl.friebel@friebelnet.de>
2018-01-30 22:47:29 +0100

    Added EnumeratorState definition.

---
+++ Whetstone/Collections/Enumerators/EnumeratorState.cs
@@ -0,0 +1,42 @@
+﻿using SC = System.Collections;
+
+using JetBrains.Annotations;
+
+namespace Whetstone.Collections.Enumerators
+{
+    /// <summary>
+    /// States of an <see cref="SC.IEnumerator"/>.
+    /// </summary>
+    [PublicAPI]
+    public enum EnumeratorState
+    {
+        /// <summary>
+        /// The enumerator has not yet been bound.
+        /// </summary>
+        /// <remarks>
+        /// No operations are valid in this state.
+        /// </remarks>
+        BeforeBinding = 0,
+        /// <summary>
+        /// The enumerator is bound and ready.
+        /// </summary>
+        /// <remarks>
+        /// All operations are valid in this state.
+        /// </remarks>
+        Bound = 1,
+        /// <summary>
+        /// The enumerator is bound but has reached the end.
+        /// </summary>
+        /// <remarks>
+        /// Specifically only <see cref="SC.IEnumerator.Reset"/> is valid in this state.
+        /// </remarks>
+        Exhausted = 2,
+        /// <summary>
+        /// The enumerator has been permanently unbound and disposed.
+        /// </summary>
+        /// <remarks>
+        /// No operations are valid in this state.
+        /// </remarks>
+        Disposed = 3
+    }
+}
